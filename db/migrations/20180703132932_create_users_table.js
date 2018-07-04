92bdd3633c1e42ac1138a24a420e493073c73b6d
Patrice <ppetit@wynd.eu>
2018-07-04 14:58:31 +0200

    DB: email unique && indexed

--- db/migrations/20180703132932_create_users_table.js
+++ db/migrations/20180703132932_create_users_table.js
@@ -3,6 +3,7 @@ exports.up = function(knex, Promise) {
         table.increments()
         table.string('name')
         table.string('email').notNullable()
+        table.unique('email', "email_idx")
         table.string("password").notNullable()
         table.timestamp("created_at").defaultTo(knex.fn.now())
         table.timestamp("updated_at").defaultTo(knex.fn.now())
